79a76939e12cd64fe06cf39226052403eff65c3e
Alex <agscheaua@gmail.com>
2026-05-10 16:33:25 +0300

    first commit, initial structure of the template do ne, i need to add the linter and formater dependen cy

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: "./src/template.html",
+    }),
+  ],
+  devServer: {
+    watchFiles: ["./src/template.html"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+ 
